58789132ecb91a3d251dcfb939b30b91355748a5
Jan Schulte <hello@unexpected-co.de>
2014-08-13 22:33:49 +0200

    add menu

---
+++ menu.rb
@@ -0,0 +1,30 @@
+def print_menu
+  puts "1. Print article list"
+  puts "2. Order article"
+  puts "3. Show shopping card"
+  puts "4. Pay"
+  puts "5. End"
+  print ">>> "
+end
+
+def main
+  while true
+    print_menu
+    choice = gets.to_i
+
+    # Use the domain classes here
+    case choice
+    when 1
+    when 2
+    when 3
+    when 4
+    when 5
+    else
+    end
+  end
+end
+
+# If the file is executed directly, then call the main method
+if __FILE__==$0
+  main
+end

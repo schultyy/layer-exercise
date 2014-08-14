77912da8cbdfb017447f844730bc34ee94bd230c
Jan Schulte <hello@unexpected-co.de>
2014-08-14 09:44:38 +0200

    Use fetch instead of []

--- article.rb
+++ article.rb
@@ -2,8 +2,8 @@ class Article
   attr_reader :id, :name, :price
 
   def initialize(article_hash)
-    @id    = article_hash[:id]
-    @name  = article_hash[:name]
-    @price = article_hash[:price]
+    @id    = article_hash.fetch(:id)
+    @name  = article_hash.fetch(:name)
+    @price = article_hash.fetch(:price)
   end
 end
